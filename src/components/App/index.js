315438f013f4b52a69e237f8ec17b7e1916d4b51
Lewis Gorman-Neale <107112881+lewisgormanneale@users.noreply.github.com>
2022-11-22 10:00:14 +0000

    initial file structure and components created

---
+++ src/components/App/index.js
@@ -0,0 +1,18 @@
+import DisplayBlog from '../DisplayBlog';
+import NavBar from '../NavBar';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <DisplayBlog />
+      <DisplayBlog />
+      <DisplayBlog />
+      <DisplayBlog />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+function NavBar() {
+  return (
+    <header className="navbar">
+        <button>Register</button>
+        <button>Login</button>
+    </header>
+  );
+}
+
+export default NavBar;
