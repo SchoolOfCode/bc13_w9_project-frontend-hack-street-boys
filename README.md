f4de186a546d1b9d49500249971f56ca30bd07f9
Faseeh Ahmed <faseeh_458@hotmail.com>
2022-11-29 16:22:22 +0000

    README added

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
-# Getting Started with Create React App
-Hello
+# SOC Study Hub
+
+This application is aimed to assist the SOC bootcampers with finding pervious content using filter options for week and topic. This overcomes a limitation with Slack which does not allow you to filter chat by topic or week. This will allow bootcampers to find previous content a lot faster and keep up with the fast phase of the course.
+
+The application allows a coach to send a post. Then the bootcampers can add comments to the posts. These comments can be deleted and editied.
+
+
+## Features
+
+- DropDown Filters
+- All CRUD functions
+
+
+## Optimizations
+
+- Seperations of concerns 
+- Sensible naming
+- Comments to improve readability
+
+## Tech Stack
+
+**Client:** React
+
+**Server:** Node, Express
+
+**DataBase:** PostgreSQL
+
+
+## Future Features
+
+Given an additional sprint we would like to add:
+
+- Login functionality
+    - To enable only logged-in users to send a post.
+    - Only enable logged in users to edit/delete their post.
+- Reset button to reset dropdown options.
+
+
+## Lessons Learned
+
+What did you learn while building this project? 
+- To be able to connet the Front-end and the Back-End 
+- Wroking as part of an agile team.
+- How to work on branches and merge them in a branch.
+
+What challenges did you face and how did you overcome them?
+- Timeframe with regards to some tasks was unclear.
+
+## Authors
+
+- [@faseehahmed1](https://github.com/faseehahmed1)
+- [@lewisgormanneale](https://www.github.com/lewisgormanneale)
+- [@Joegitrussell](https://www.github.com/Joegitrussell)
+- [@Mohamed1104](https://www.github.com/Mohamed1104)
+
---
+++ src/components/Filter/index.js
@@ -0,0 +1,141 @@
+import React, { useEffect } from 'react'
+import Select from "react-select";
+import { useState } from 'react';
+
+export default function Filter({ postDB, setPostDB }) {
+  const [topicOption, setTopicOption] = useState("");
+  const [weekOption, setWeekOption] = useState("");
+
+  useEffect(() => {
+    //Creates new object with label needed for Topic DropDown to work
+    const arrTopic = [];
+    const arrWeek = [];
+    const result = postDB;
+    result.map((post) => {
+      return arrTopic.push({ value: post.topic, label: post.topic });
+    });
+
+    result.map((post) => {
+      return arrWeek.push({
+        value: post.week_number,
+        label: post.week_number,
+      });
+    });
+
+    //Removes duplicates from Topic dropdown
+    const uniqueTopics = [];
+    const unique = arrTopic.filter((element) => {
+      const isDuplicate = uniqueTopics.includes(element.label);
+      if (!isDuplicate) {
+        uniqueTopics.push(element.label);
+        return true;
+      }
+      return false;
+    });
+
+    //Removes duplicates from Week dropdown
+    const uniqueWeeks = [];
+    const uniqueWeek = arrWeek.filter((element) => {
+      const isDuplicate = uniqueWeeks.includes(element.label);
+      if (!isDuplicate) {
+        uniqueWeeks.push(element.label);
+        return true;
+      }
+      return false;
+    });
+
+    //Sorts Week Dropdown by size
+    setWeekOption(
+      uniqueWeek.sort((a, b) => {
+        return a.label - b.label;
+      })
+    );
+
+    //Sorts Topic Dropdown alphabetically
+    setTopicOption(
+      unique.sort((a, b) => {
+        return a.label.toLowerCase() > b.label.toLowerCase()
+          ? 1
+          : a.label.toLowerCase() < b.label.toLowerCase()
+          ? -1
+          : 0;
+      })
+    );
+  }, [postDB]);
+
+  //Filter out the selected topic dropdown in postDB
+  const handleTopic = async (selected) => {
+    const response = await fetch(`http://localhost:3001/api/posts`);
+    const data = await response.json();
+    const payload = data.payload;
+    const dropDownTopic = payload.filter((obj) => {
+      return obj.topic === selected.label;
+    });
+    setPostDB(dropDownTopic);
+    const arrWeek = [];
+    dropDownTopic.map((post) => {
+      return arrWeek.push({
+        value: post.week_number,
+        label: post.week_number,
+      });
+    });
+    const uniqueWeeks = [];
+    const uniqueWeek = arrWeek.filter((element) => {
+      const isDuplicate = uniqueWeeks.includes(element.label);
+      if (!isDuplicate) {
+        uniqueWeeks.push(element.label);
+        return true;
+      }
+      return false;
+    });
+    setWeekOption(
+      uniqueWeek.sort((a, b) => {
+        return a.label - b.label;
+      })
+    );
+  };
+
+  //Filters out the selected week dropdown in postDB
+  const handleWeek = async (selected) => {
+    const response = await fetch(`http://localhost:3001/api/posts`);
+    const data = await response.json();
+    const payload = data.payload;
+    const dropDownWeek = payload.filter((obj) => {
+      return obj.week_number === Number(selected.label);
+    });
+    setPostDB(dropDownWeek);
+    const arrTopic = [];
+    dropDownWeek.map((post) => {
+      return arrTopic.push({ value: post.topic, label: post.topic });
+    });
+    const uniqueTopics = [];
+    const unique = arrTopic.filter((element) => {
+      const isDuplicate = uniqueTopics.includes(element.label);
+      if (!isDuplicate) {
+        uniqueTopics.push(element.label);
+        return true;
+      }
+      return false;
+    });
+    setTopicOption(
+      unique.sort((a, b) => {
+        return a.label.toLowerCase() > b.label.toLowerCase()
+          ? 1
+          : a.label.toLowerCase() < b.label.toLowerCase()
+          ? -1
+          : 0;
+      })
+    );
+  };
+
+  return (
+    <div>
+      <Select options={weekOption} placeholder="Week" onChange={handleWeek} />
+      <Select
+        options={topicOption}
+        placeholder="Topic"
+        onChange={handleTopic}
+      />
+    </div>
+  );
+}
